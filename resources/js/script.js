ad49059ddc180c6c2c75908cc93e6f29d53cce8c
Zara <zl.zlatkova@gmail.com>
2023-05-23 14:14:55 +0200

    completed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,13 @@
-const form = document.querySelector('.form-group');
+const form = document.querySelector(".form-group");
 const button = form.querySelector('[type="submit"]');
 
+//error_text
 const message = `* Please provide a valid email address`;
-const error = document.createElement('p');
-error.classList.add('error_text');
+const error = document.createElement("p");
+error.classList.add("error_text");
 error.textContent = message;
 
 //listening for submit an event
-
 function checkEmail(e) {
   //prevent default area
   e.preventDefault();
@@ -15,41 +15,33 @@ function checkEmail(e) {
   //grab the value of the email
   const email = e.target.querySelector('[type="email"]').value;
 
+  // standard html notification
+  if (!validateEmail(email)) {
+    form.classList.add("error");
+    form.insertBefore(error, button);
+  } else {
+    form.classList.remove("error");
+    form.removeChild(error);
+  }
+  console.log(email);
+
   /* alternative -> alert warning popup notification
 
   if (!validateEmail(email)) {
     alert ("Please provide a valid email address");
     return false;
-
   } else {
     alert ("Valid email address!");
     return true;
   }
   */
-
-
-  // standard html notification
-  if (!validateEmail(email)) {
-    form.classList.add('error');
-    form.insertBefore(error, button);
-
-
-
-  } else {
-    form.classList.remove('error');
-    form.removeChild(error);
-
-  }
-  console.log(email);
-
 }
 
 //regex mail validation
 const validateEmail = (email) => {
-  const regex = /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,3}(?:\.[a-z]{2,3})?$/gm;
+  const regex =
+    /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,3}(?:\.[a-z]{2,3})?$/gm;
   return regex.test(String(email).toLocaleLowerCase());
-
 };
 
-
-form.addEventListener('submit', checkEmail);
+form.addEventListener("submit", checkEmail);
