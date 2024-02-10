a25c6513b0339abbdc94105b42dba495d6caff92
Zara <zl.zlatkova@gmail.com>
2024-02-10 17:54:28 +0100

    create an alert msg

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,40 +1,67 @@
-const form = document.querySelector(".form-group");
-const button = form.querySelector('[type="submit"]');
+const formRef = document.querySelector(".form-group");
+const emailRef = document.querySelector("#email");
+const buttonRef = document.querySelector('[type="submit"]');
 
-//error_text
-const message = `* Please provide a valid email address`;
-const error = document.createElement("p");
-error.classList.add("error_text");
-error.textContent = message;
+formRef.addEventListener("submit", checkEmail);
 
-//listening to the submit event
-function checkEmail(e) {
-  //prevent default area
-  e.preventDefault();
+function checkEmail(evn) {
+  evn.preventDefault();
 
-  //grab the value of the email
-  const email = e.target.querySelector('[type="email"]').value;
+  const email = emailRef.value;
 
-  /* standard html notification
-  if (!validateEmail(email)) {
-    form.classList.add("error");
-    form.insertBefore(error, button);
-  } else {
-    form.classList.remove("error");
-    form.removeChild(error);
+  if (!email) {
+    return;
   }
-  console.log(email);
-  */
-  // alternative -> alert warning popup notification
+
+  const liElement = document.createElement("div");
+  liElement.className = "alert";
+
 
   if (!validateEmail(email)) {
-    alert ("Please provide a valid email address");
-    return false;
+    const error = genElements("p", liElement, `* Please provide a valid email address.`, "error_text");
+    formRef.appendChild(error);
+
+    formRef.classList.remove("notify");
+    formRef.classList.add("error");
+    formRef.insertBefore(error, buttonRef);
+
+    setTimeout(() => removeAlert(error), 1600);
+
   } else {
-    alert ("Valid email address!");
-    return true;
+    const notify = genElements("p", liElement, `* We're happy to see you've joined us!`, "notify_text");
+    formRef.appendChild(notify);
+
+    formRef.classList.remove("error");
+    formRef.classList.add("notify");
+    formRef.insertBefore(notify, buttonRef);
+
+    //Hide the alert after 1000ms (the same amount of milliseconds it takes to fade out)
+    setTimeout(() => removeAlert(notify), 1000);
+
   }
-  
+
+}
+
+//remove dom element
+function removeAlert(alertEl) {
+  alertEl.remove();
+  formRef.className = "form-group";
+  emailRef.value = "";
+
+}
+
+//creating a DOM element
+function genElements(tag, parent, content, className) {
+  const element = document.createElement(tag);
+  if (content) {
+    element.textContent = content;
+  }
+  if (className) {
+    element.className = className;
+  }
+
+  parent.appendChild(element);
+  return element;
 }
 
 //regex mail validation
@@ -43,5 +70,3 @@ const validateEmail = (email) => {
     /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,3}(?:\.[a-z]{2,3})?$/gm;
   return regex.test(String(email).toLocaleLowerCase());
 };
-
-form.addEventListener("submit", checkEmail);
